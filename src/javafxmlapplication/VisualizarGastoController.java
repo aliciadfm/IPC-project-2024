387e015273acaf3523b8281ac8573855c54eb7ee
unaydf <unaydavidfigueroa@gmail.com>
2024-05-18 16:22:46 +0200

    SceneController implementado en el resto de clases

--- src/javafxmlapplication/VisualizarGastoController.java
+++ src/javafxmlapplication/VisualizarGastoController.java
@@ -4,9 +4,17 @@
  */
 package javafxmlapplication;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,5 +30,40 @@ public class VisualizarGastoController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    private void CancelarVisualizarGasto(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("VisualizarGastos.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    private void PulsarEditarGasto(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("EditarGasto.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void CancelarEditarGasto(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("VisualizarGasto.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    private void AceptarEditarGasto(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("VisualizarGasto.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    
     
 }
--- src/javafxmlapplication/VisualizarGastosController.java
+++ src/javafxmlapplication/VisualizarGastosController.java
@@ -4,9 +4,17 @@
  */
 package javafxmlapplication;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,5 +30,14 @@ public class VisualizarGastosController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         
     }    
+
+    @FXML
+    private void VolverVisualizarGastos(ActionEvent event) throws IOException {
+        Parent root = FXMLLoader.load(getClass().getResource("ContenedorPrincipal.fxml"));
+        Stage stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
     
 }
